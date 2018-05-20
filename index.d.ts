ed419b11e4a664f3456038132a7237438a83b57d
HashimotoYT <hashimoto.stream@gmail.com>
2018-05-20 21:38:01 +0300

    Semicolon and docs

--- index.d.ts
+++ index.d.ts
@@ -4,10 +4,22 @@
 
 declare function updater(
     opts?: {
-        repo?: string,
-        host?: string,
-        updateInterval?: string,
-        logger?: Object
+        /**
+         * @param {String} repo A GitHub repository in the format `owner/repo`. Defaults to your `package.json`'s `"repository"` field
+         */
+        repo?: string;
+        /**
+         * @param {String} host Defaults to `https://update.electronjs.org`
+         */
+        host?: string;
+        /**
+         * @param {String} updateInterval How frequently to check for updates. Defaults to `10 minutes`. Minimum allowed interval is `5 minutes`.
+         */
+        updateInterval?: string;
+        /**
+         * @param {Object} logger A custom logger object that defines a `log` function. Defaults to `console`. See electron-log, a module that aggregates logs from main and renderer processes into a single file.
+         */
+        logger?: Object;
     }
 ): void
 
