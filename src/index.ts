443ba1d33ac56d17c4b6930e4e2e2841b2f119ee
rhy3h <rhy3htw@gmail.com>
2025-02-25 14:00:34 +0800

    feat: support not to auto check update & manual check update

--- src/index.ts
+++ src/index.ts
@@ -96,6 +96,11 @@ export interface IUpdateElectronAppOptions<L = ILogger> {
    */
   readonly updateInterval?: string;
   /**
+   * @param {Boolean} autoCheck Decides whether to automatically check for updates
+   *                            Defaults to `true`.
+   */
+  readonly autoCheck?: boolean;
+  /**
    * @param {Object} logger A custom logger object that defines a `log` function.
    *                        Defaults to `console`. See electron-log, a module
    *                        that aggregates logs from main and renderer processes into a single file.
@@ -152,7 +157,7 @@ export function updateElectronApp(opts: IUpdateElectronAppOptions = {}) {
 }
 
 function initUpdater(opts: ReturnType<typeof validateInput>) {
-  const { updateSource, updateInterval, logger } = opts;
+  const { updateSource, updateInterval, autoCheck, logger } = opts;
 
   // exit early on unsupported platforms, e.g. `linux`
   if (!supportedPlatforms.includes(process?.platform)) {
@@ -241,11 +246,27 @@ function initUpdater(opts: ReturnType<typeof validateInput>) {
     );
   }
 
-  // check for updates right away and keep checking later
-  autoUpdater.checkForUpdates();
-  setInterval(() => {
+  if (autoCheck) {
+    // check for updates right away and keep checking later
     autoUpdater.checkForUpdates();
-  }, ms(updateInterval));
+    setInterval(() => {
+      autoUpdater.checkForUpdates();
+    }, ms(updateInterval));
+  }
+}
+
+export function checkForUpdates() {
+  return new Promise((resolve) => {
+    autoUpdater.checkForUpdates();
+
+    autoUpdater.once('update-available', () => {
+      resolve(true);
+    });
+
+    autoUpdater.once('update-not-available', () => {
+      resolve(false);
+    });
+  });
 }
 
 /**
@@ -296,11 +317,12 @@ function validateInput(opts: IUpdateElectronAppOptions) {
   const defaults = {
     host: 'https://update.electronjs.org',
     updateInterval: '10 minutes',
+    autoCheck: true,
     logger: console,
     notifyUser: true,
   };
 
-  const { host, updateInterval, logger, notifyUser, onNotifyUser } = Object.assign(
+  const { host, updateInterval, autoCheck, logger, notifyUser, onNotifyUser } = Object.assign(
     {},
     defaults,
     opts,
@@ -348,5 +370,5 @@ function validateInput(opts: IUpdateElectronAppOptions) {
 
   assert(logger && typeof logger.log, 'function');
 
-  return { updateSource, updateInterval, logger, notifyUser, onNotifyUser };
+  return { updateSource, updateInterval, autoCheck, logger, notifyUser, onNotifyUser };
 }
